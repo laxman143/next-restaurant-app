bed4be7fa9c4a5330345b4a47a00edb622c2e632
laxman143 <laxmanprajapati143@gmail.com>
2025-05-22 09:50:08 +0530

    Resolved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import {  Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Notification from "./components/Notification";
-  
+
 
 const inter= Inter({ subsets : ['latin']})
 
